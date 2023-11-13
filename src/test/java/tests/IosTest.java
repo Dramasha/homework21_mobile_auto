3eeca889e1b5f71e9dc2194b3e6500bf3f732a27
Dramasha <Petyukovsv@gmail.com>
2023-11-13 16:29:25 +0300

    add

--- src/test/java/tests/IosTest.java
+++ src/test/java/tests/IosTest.java
@@ -1,4 +1,34 @@
 package tests;
 
-public class IosTest {
+import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Selenide.$;
+import static io.appium.java_client.AppiumBy.id;
+import static io.qameta.allure.Allure.step;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class IosTest extends BaseTest {
+    @Test
+    @Tag("ios")
+    @DisplayName("Checking text input and display in UI Elements")
+    void enterTextTest() {
+
+        step("Click on button Text", () -> {
+            $(id("Text Button")).click();
+        });
+
+        step("Click on field Enter a text", () -> {
+            $(id("Text Input")).click();
+        });
+
+        step("Input in field 'Enter a text' text 'I want to learn Java quite good' and press Enter", () -> {
+            $(id("Text Input")).sendKeys("I want to learn Java quite good");
+            $(id("Text Input")).pressEnter();
+        });
+
+        step("Checking the display of the result with the specified text", () -> {
+            assertThat($(id("Text Output")).getText())
+                    .isEqualTo("I want to learn Java quite good");
+        });
+    }
 }
